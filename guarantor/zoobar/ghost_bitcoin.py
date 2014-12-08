c27a992b5edf4e348f1c9fdd4cf5be8b21275a8f
Brendan Chang <bschang@mit.edu>
2014-12-08 22:01:44 +0000

    added getreceivedbyaddress wrapper in ghost_bitcoin and replaced proxy calls in deposit with wrappers

--- guarantor/zoobar/ghost_bitcoin.py
+++ guarantor/zoobar/ghost_bitcoin.py
@@ -10,6 +10,13 @@ def getnew():
 	proxy = rpc.Proxy()
 	newaddress = proxy.getnewaddress()
 	print(newaddress)
+    return newaddress
+
+def getreceivedbyaddress(address):
+    proxy = rpc.Proxy()
+    amount = proxy.getreceivedbyaddress(address)
+    print(amount)
+    return amount
 
 def send_bitcoins(address,amount):
 	proxy = rpc.Proxy()
