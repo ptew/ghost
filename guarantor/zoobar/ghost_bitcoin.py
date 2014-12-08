4e0d4f9ea3ae257e1541ecca69431930c1c6346c
Parker Tew <ptew@mit.edu>
2014-12-08 15:53:59 -0500

    chanched ghost_bitcoin to python2 from python3

--- guarantor/zoobar/ghost_bitcoin.py
+++ guarantor/zoobar/ghost_bitcoin.py
@@ -1,36 +1,26 @@
-#!/usr/bin/python3
-
-import sys
-if sys.version_info.major < 3:
-    sys.stderr.write('Sorry, Python 3.x required by this example.\n')
-    sys.exit(1)
+#!/usr/bin/python
 
 import hashlib
-
-import bitcoin
-import ghost_blockchain
-import bitcoin.rpc
-from bitcoin import SelectParams
-
+from bitcoin import rpc, SelectParams
 
 # SelectParams('mainnet')
 SelectParams('testnet')
 
 def getnew():
-	proxy = bitcoin.rpc.Proxy()
+	proxy = rpc.Proxy()
 	newaddress = proxy.getnewaddress()
 	print(newaddress)
 
 def send_bitcoins(address,amount):
-	proxy = bitcoin.rpc.Proxy()
+	proxy = rpc.Proxy()
 	transaction = proxy.sendtoaddress(address, amount)
 	print(transaction)
 	return transaction
 
 def get_balance():
-	proxy = bitcoin.rpc.Proxy()
+	proxy = rpc.Proxy()
 	balance = proxy.getbalance()
 	print("balance: " + str(balance))
 	return float(balance)
-
+# get_balance()
 # send_bitcoins('mmHExc9mUChgAsqBVNZ1WVpFa2Do2r9gnR', get_balance()/4)
