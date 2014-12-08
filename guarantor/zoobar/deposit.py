f4eba14e79002b612021ed8acaa17353bafd600f
Brendan Chang <bschang@mit.edu>
2014-12-08 00:48:17 +0000

    updated deposit processing method for updated one-time-use wallet design

--- guarantor/zoobar/deposit.py
+++ guarantor/zoobar/deposit.py
@@ -3,32 +3,56 @@
 from jsonrpc import ServiceProxy
 import time
 
-# use os.urandom? prob doesn't matter for this...
-import random
-
-def process_deposits(transactions):
-    for t in transactions:
-        if t["category"] == "receive":
-            # extract user, deposit id, amount
-            ### have block and account, what to do? where is deposit id?
-            ### how to map deposit id to user?
-            # verify deposit id
-            # increase user balance by amount
-            # generate new deposit id
-            break
+from zoodb import *
+import ghost_blockchain
 
 def check_for_deposits():
     # username:password@address
     access = ServiceProxy('guarantor bitcoin wallet/block?')
     acct = 'specific bitcoin account?'
-    current = 0
-    while True:
-        # will get up to count = 25 (by default) of the most recent transactions
-        new_transactions = access.listtransactions(account = acct, offset = current)["transactions"]
-        process_deposits(new_transactions)
+    wallet_db = wallet_setup()
+    
+    # TODO: make sure that deposit service has bank access for updating user balances
+    bank_db = bank_setup()
 
-        # update offset
-        current += len(new_transactions)
+    while True:
+        wallets = wallet_db.query(Wallet).all()
+        for wallet in wallets:
+            # TODO: find some way to get transactions for the wallet
+            
+            # should assume that each of these one-time use wallets only has a single transaction
+            if (len(wallet.transactions) > 0):
+                transaction = wallet.transactions[0]
+                
+                # send bitcoins from temp wallet to guarantor wallet
+                # TODO: replace arguments with correct method calls or values
+                ghost_blockchain.send_bitcoins(wallet.identifier, wallet.wallet_creds, guarantor.address, transaction.amount) 
+
+                # find user and update user balance
+                # wallet address should be one-to-one mapping for user
+                user = bank_db.query(Bank).filter(Bank.bank_id == wallet.bank_id)[0]
+                user.balance += transaction.amount
+
+                # commit changes to user
+                bank_db.commit()
+
+                # get new wallet and update wallet_db
+                # TODO: update with correct password and api code
+                new_wallet_info = ghost_blockchain.create_wallet(some_password, some_api_code)
+                new_wallet = Wallet()
+                new_wallet.wallet_address = new_wallet_info.address
+                new_wallet.wallet_creds = some_password
+                new_wallet.bank_id = wallet.bank_id
+                wallet_db.add(new_wallet)
+
+                # doesn't the user need to know what one-time-use wallet to deposit to?
+
+                # remove current wallet from wallet_db
+                # TODO: not sure if this is the right command, since remove only works on a query
+                wallet.remove(False)
         
+        # commit all changes after all wallets have been processed
+        wallet_db.commit()
+ 
         # current pause is 5 minutes
         time.sleep(300)
