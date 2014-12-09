602c8962b32a655722d60f67ab53610851af8889
Ada Taylor <adat@mit.edu>
2014-12-09 00:41:47 -0500

    Fleshed out methods for customer sending transaction

--- customer/transaction.py
+++ customer/transaction.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
-import sys
+import checkbits
+import random
 
 #Need to change transaction id for every test
 #merchant needs to check for the same transaction_id
-transaction_id = 
+transaction_id = 123456789101112131415161718192021222324252627282930 
 
 #1 Satoshi
 amount = .00000001
@@ -13,10 +14,26 @@ amount = .00000001
 merchant_address = "17tezGZcySJeDWsKYBsDubCEQZWM8YgnKT"
 bulletin = "54.68.222.230"
 
+check = {}
+check['transaction_id'] = transaction_id
+check['amount'] = amount
+check['merchant_address'] = merchant_address
+check['bulletin'] = bulletin
+check['randomness'] = random.getrandbits(40)
+#TODO padding?
+
+#TODO set up working signing and decrypting keys
+signing_key = 1
+encrypting_key = 1
+
+#Guarantor address
+guarantor_address = "54.69.86.196"
 
 
+echo check
+echo "Encrypting check"
+encrypted_check = checkbits.make_check(check, signing_key, encrypting_key)
 
-
-
+#TODO send encrypted_check to guarantor_address
 
 
