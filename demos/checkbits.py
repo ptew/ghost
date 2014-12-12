4d4f8dc165588acc9bee767c126906f86c6ac56d
httpd <ada@idu.com>
2014-12-12 11:47:20 +0000

    Call to encrypt check streamlined, signature incorporated into encryption

--- demos/checkbits.py
+++ demos/checkbits.py
@@ -9,10 +9,7 @@ import json
 import os
 import M2Crypto
 
-#Test keys for Customer and Guarantor set up here
-from setup_test_keys import *
-
-def make_check(check, signing_private_key, encrypting_public_key):
+def make_check(check, customer, encrypting_public_key):
 
     plaintext_check = json.dumps(check)
     print "JSON CHECK"
@@ -23,34 +20,19 @@ def make_check(check, signing_private_key, encrypting_public_key):
     elif ('amount' not in check):
 	raise ValueError("Amount not found")
 
-    #signed_check = sign_check(plaintext_check, signing_private_key)
-
-    encrypted_check = encrypt_check(plaintext_check, encrypting_public_key)
-
-    return encrypted_check
+    return encrypt_check(plaintext_check, customer, encrypting_public_key)
     
-def sign_check(check, key):
-    rng = Random.new().read
-    hash = MD5.new(check).digest()
-    signature = key.sign(hash, rng)
+def encrypt_check(check, customer, guarantor_public_key):
 
-    print "SIGNATURE"
-    print signature[0]
-    return str(signature[0])
-
-def encrypt_check(check, key):
-
-    WriteRSA = M2Crypto.RSA.load_pub_key('Guarantor-public.pem')
-    CipherText = WriteRSA.public_encrypt (check, M2Crypto.RSA.pkcs1_oaep_padding)
-    print "\nAlice's encrypted message to Bob:"
+    CipherText = guarantor_public_key.public_encrypt (check, M2Crypto.RSA.pkcs1_oaep_padding)
+    print "\nCustomer's encrypted message to Guarantor:"
     print CipherText.encode ('base64')
     
     MsgDigest = M2Crypto.EVP.MessageDigest ('sha1')
     MsgDigest.update(CipherText)
 
-    Signature = Customer.sign_rsassa_pss(MsgDigest.digest())
-    # 2) Print the result
-    print "Alice's signature for this message:"
+    Signature = customer.sign_rsassa_pss(MsgDigest.digest())
+    print "Customer's signature for this message:"
     print Signature.encode ('base64')
 
 def decrypt_check(check, decrypting_key):
